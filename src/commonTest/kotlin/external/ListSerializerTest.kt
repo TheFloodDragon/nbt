0bc3479c8f0ee8cc05706c45326ac163fe9e398c
Ben Woodworth <ben@benwoodworth.com>
2024-08-19 18:20:29 -0400

    Make the `ListSerializerTest` suite more thorough
    Change the first test to focus only on whether an NbtList is
    serialized without worrying about the content.
    
    Add test to cover lists containing any of the serializable type edge
    cases.
    
    Change list-of-lists test to cover the two lists containing different
    types.

--- src/commonTest/kotlin/external/ListSerializerTest.kt
+++ src/commonTest/kotlin/external/ListSerializerTest.kt
@@ -1,8 +1,13 @@
 package net.benwoodworth.knbt.external
 
+import kotlinx.serialization.KSerializer
 import kotlinx.serialization.builtins.*
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
 import net.benwoodworth.knbt.*
 import net.benwoodworth.knbt.test.parameterizeTest
+import net.benwoodworth.knbt.test.parameters.parameterOfSerializableTypeEdgeCases
 import net.benwoodworth.knbt.test.parameters.parameterOfVerifyingNbt
 import kotlin.test.Test
 import kotlin.test.assertContentEquals
@@ -13,9 +18,35 @@ class ListSerializerTest {
         val nbt by parameterOfVerifyingNbt()
 
         nbt.verifyEncoderOrDecoder(
-            ListSerializer(Int.serializer()),
-            listOf(1, 2, 3),
-            NbtList(listOf(1, 2, 3).map(::NbtInt)),
+            ListSerializer(Unit.serializer()),
+            listOf(),
+            NbtList(listOf()),
+            testDecodedValue = { value, decodedValue ->
+                assertContentEquals(value, decodedValue, "decodedValue")
+            }
+        )
+    }
+
+    @Test
+    @OptIn(UnsafeNbtApi::class)
+    fun should_serialize_List_of_value_correctly() = parameterizeTest {
+        val nbt by parameterOfVerifyingNbt()
+        val serializableType by parameterOfSerializableTypeEdgeCases()
+
+        val serializer = object : KSerializer<Unit> {
+            override val descriptor: SerialDescriptor = serializableType.baseDescriptor
+
+            override fun serialize(encoder: Encoder, value: Unit): Unit =
+                serializableType.encodeValue(encoder, descriptor)
+
+            override fun deserialize(decoder: Decoder): Unit =
+                serializableType.decodeValue(decoder, descriptor)
+        }
+
+        nbt.verifyEncoderOrDecoder(
+            ListSerializer(serializer),
+            listOf(Unit),
+            NbtList(listOf(serializableType.valueTag)),
             testDecodedValue = { value, decodedValue ->
                 assertContentEquals(value, decodedValue, "decodedValue")
             }
@@ -31,7 +62,7 @@ class ListSerializerTest {
             listOf(listOf(1.toByte()), listOf()),
             buildNbtList<NbtList<*>> {
                 addNbtList<NbtByte> { add(1.toByte()) }
-                addNbtList<Nothing> { }
+                addNbtList<NbtInt> { add(1) } // Different in case the entry type carries over somehow
             },
             testDecodedValue = { value, decodedValue ->
                 assertContentEquals(value, decodedValue, "decodedValue")
