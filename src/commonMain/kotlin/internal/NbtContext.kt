9f2dd74af385bba56f6ded0342b1689a7a71b9d3
Ben Woodworth <ben@benwoodworth.com>
2024-09-01 16:57:21 -0400

    WIP Only encode `NbtName` at root

--- src/commonMain/kotlin/internal/NbtContext.kt
+++ src/commonMain/kotlin/internal/NbtContext.kt
@@ -33,8 +33,12 @@ internal data object EmptyNbtContext : NbtContext {
  */
 internal class SerializationNbtContext : NbtContext {
     private var currentDescriptor: SerialDescriptor? = null
+    private var structureNesting = 0
 
     override fun getPath(): NbtPath? = null // TODO
+
+    val isSerializingRootValue: Boolean
+        get() = structureNesting == 0
 
     /**
      * Decorates calls to [SerializationStrategy.serialize] and [DeserializationStrategy.deserialize] so that serializer
@@ -50,6 +54,14 @@ internal class SerializationNbtContext : NbtContext {
         return block().also {
             currentDescriptor = previousDescriptor
         }
+    }
+
+    fun onBeginStructure() {
+        structureNesting++
+    }
+
+    fun onEndStructure() {
+        structureNesting--
     }
 
     fun checkDynamicallySerializingNbtName() {
