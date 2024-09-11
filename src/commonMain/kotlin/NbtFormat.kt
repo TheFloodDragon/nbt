793eb2b98d0c9fbe35750c856d86e6e8833d8ac6
Ben Woodworth <ben@benwoodworth.com>
2024-09-10 22:08:28 -0400

    WIP Format-specific `encodeTo`/`decodeFromNbtTag` functions
    Nbt from any NbtFormat

--- src/commonMain/kotlin/NbtFormat.kt
+++ src/commonMain/kotlin/NbtFormat.kt
@@ -12,6 +12,10 @@ public abstract class NbtFormat internal constructor() : SerialFormat {
     /**
      * Serializes the given [value] into an equivalent [NbtTag] using the given [serializer].
      *
+     * This method is considered unsafe because it returns a named [NbtTag], even though this [NbtFormat] may not have a
+     * root name. [NbtFormat]s with unnamed root tags should discard the returned [NbtNamed.name][name], and restrict
+     * the returned tag type to those that are supported.
+     *
      * @throws [SerializationException] if the given value cannot be serialized to NBT.
      */
     protected fun <T> encodeToNbtTagUnsafe(serializer: SerializationStrategy<T>, value: T): NbtNamed<NbtTag> {
@@ -26,6 +30,10 @@ public abstract class NbtFormat internal constructor() : SerialFormat {
 
     /**
      * Deserializes the given [tag] into a value of type [T] using the given [deserializer].
+     *
+     * This method is considered unsafe because it accepts a root tag name and any tag type, and the not all be
+     * supported by this [NbtFormat]. [NbtFormat]s should restrict calls to this method so only valid [tag]s are used
+     * for deserialization.
      *
      * @throws [SerializationException] if the given NBT tag is not a valid NBT input for the type [T].
      * @throws [IllegalArgumentException] if the decoded input cannot be represented as a valid instance of type [T].
