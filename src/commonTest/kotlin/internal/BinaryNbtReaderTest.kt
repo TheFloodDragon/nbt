BenWoodworth/knbt

Use Parameterize instead of Kotest property testing
Kotest hasn't been a good fit for the kind of thorough testing that this
 project has been demanding. The Parameterize DSL allows edge case
 testing to be more modular with the way parameters are dynamically
 declared, and as a result, the code becomes much clearer without any
 nesting or up-front property declarations.

Changes:
- Replace Kotest generators with more targeted/precise parameter
  functions.
  - `Arb.*()` -> `parameterOf*EdgeCases()`
  - `Exhaustive.*()` -> `parameterOf*()`
- Combine similar tests into single parameterized tests.
  - `BinaryNbtWriterTest.should_fail_when_decoding_*`
  - `SerialDescriptorTest.kind_for_*_should_be_*`
  - `StringifiedNbtReaderTest.should_read_*_correctly`
- Convert tests that behave like parameterized tests.
  - Usages of the old `parameterize(list) {}` function.
  - Different test cases in `checkAll` and `forEach` loops.
  - Repeated calls to test case functions like `testResource`,
    `testFile`, and `check`.
- Remove the `Arb.nbt*Array()` and `Arb.nbtCompound()` functions.
  - Their usages were all made more targeted, and didn't benefit from
    the depth of values that these provided.
- Remove `nbtTagEdgeCases` in favor of edge cases by `NbtTag`'s type, or
  other type-specific edge cases.
- Remove kotlinx-coroutines-test, since it was only needed for Kotest.
- Enable `linuxArm64` and `watchosDeviceArm64` targets, since they're
  now supported by all dependencies.

src/commonTest/kotlin/internal/BinaryNbtReaderTest.kt
@@ -1,5 +1,6 @@
 package net.benwoodworth.knbt.internal
 
+import com.benwoodworth.parameterize.parameter
 import kotlinx.serialization.decodeFromByteArray
 import kotlinx.serialization.encodeToByteArray
 import net.benwoodworth.knbt.*
@@ -7,7 +8,9 @@ import net.benwoodworth.knbt.NbtVariant.Java
 import net.benwoodworth.knbt.okio.decodeFromBufferedSource
 import net.benwoodworth.knbt.test.TestSource
 import net.benwoodworth.knbt.test.file.nbtFiles
-import net.benwoodworth.knbt.test.parameterize
+import net.benwoodworth.knbt.test.filter
+import net.benwoodworth.knbt.test.parameterizeTest
+import net.benwoodworth.knbt.test.parameters.parameterOfNbtCompressions
 import okio.buffer
 import okio.use
 import kotlin.test.*
@@ -15,88 +18,84 @@ import kotlin.test.*
 @OptIn(OkioApi::class)
 class BinaryNbtReaderTest {
     @Test
-    fun should_decode_to_class_correctly() = parameterize(nbtFiles) {
+    fun should_decode_to_class_correctly() = parameterizeTest {
+        val nbtFile by parameter(nbtFiles)
+
         assertEquals(
-            expected = value,
-            actual = asSource().use { source ->
-                nbt.decodeFromBufferedSource(valueSerializer, source.buffer())
+            expected = nbtFile.value,
+            actual = nbtFile.asSource().use { source ->
+                nbtFile.nbt.decodeFromBufferedSource(nbtFile.valueSerializer, source.buffer())
             },
         )
     }
 
     @Test
-    fun should_decode_to_NbtTag_correctly() = parameterize(nbtFiles) {
+    fun should_decode_to_NbtTag_correctly() = parameterizeTest {
+        val nbtFile by parameter(nbtFiles)
+
         assertEquals(
-            expected = nbtTag,
-            actual = asSource().use { source ->
-                nbt.decodeFromBufferedSource(source.buffer())
+            expected = nbtFile.nbtTag,
+            actual = nbtFile.asSource().use { source ->
+                nbtFile.nbt.decodeFromBufferedSource(source.buffer())
             },
         )
     }
 
     @Test
-    fun should_not_read_more_from_source_than_necessary() = parameterize(nbtFiles) {
-        TestSource(asSource()).use { source ->
-            nbt.decodeFromBufferedSource<NbtTag>(source.buffer())
+    fun should_not_read_more_from_source_than_necessary() = parameterizeTest {
+        val nbtFile by parameter(nbtFiles)
+
+        TestSource(nbtFile.asSource()).use { source ->
+            nbtFile.nbt.decodeFromBufferedSource<NbtTag>(source.buffer())
             assertFalse(source.readPastEnd)
         }
     }
 
     @Test
-    fun should_not_close_source() = parameterize(nbtFiles) {
-        TestSource(asSource()).use { source ->
-            nbt.decodeFromBufferedSource<NbtTag>(source.buffer())
+    fun should_not_close_source() = parameterizeTest {
+        val nbtFile by parameter(nbtFiles)
+
+        TestSource(nbtFile.asSource()).use { source ->
+            nbtFile.nbt.decodeFromBufferedSource<NbtTag>(source.buffer())
             assertFalse(source.isClosed)
         }
     }
 
     @Test
-    fun should_fail_with_incorrect_NbtCompression_and_specify_mismatched_compressions() {
-        data class Parameters(
-            val configuredCompression: NbtCompression,
-            val fileCompression: NbtCompression,
-        )
-
+    fun should_fail_with_incorrect_NbtCompression_and_specify_mismatched_compressions() = parameterizeTest {
         val data = buildNbtCompound("root") {
             put("string", "String!")
         }
 
-        val compressions = listOf(
-            NbtCompression.None,
-            NbtCompression.Gzip,
-            NbtCompression.Zlib,
-        )
+        val configuredCompression by parameterOfNbtCompressions()
 
-        val mismatchedCompressions = compressions
-            .flatMap { a -> compressions.map { b -> Parameters(a, b) } }
-            .filter { (a, b) -> a !== b }
+        val fileCompression by parameterOfNbtCompressions()
+            .filter { it != configuredCompression }
 
-        parameterize(mismatchedCompressions, { "Configured: $configuredCompression, File: $fileCompression" }) {
-            val decodingNbt = Nbt {
-                variant = Java
-                compression = configuredCompression
-            }
-
-            val encodingNbt = Nbt(decodingNbt) {
-                compression = fileCompression
-            }
-
-            val encoded = encodingNbt.encodeToByteArray<NbtTag>(data)
-
-            val error = assertFailsWith<NbtDecodingException> {
-                decodingNbt.decodeFromByteArray<NbtTag>(encoded)
-            }
-
-            val errorMessage = error.message
-            assertNotNull(errorMessage)
-            assertContains(
-                errorMessage,
-                configuredCompression.toString(),
-            )
-            assertContains(
-                errorMessage,
-                fileCompression.toString(),
-            )
+        val decodingNbt = Nbt {
+            variant = Java
+            compression = configuredCompression
         }
+
+        val encodingNbt = Nbt(decodingNbt) {
+            compression = fileCompression
+        }
+
+        val encoded = encodingNbt.encodeToByteArray<NbtTag>(data)
+
+        val error = assertFailsWith<NbtDecodingException> {
+            decodingNbt.decodeFromByteArray<NbtTag>(encoded)
+        }
+
+        val errorMessage = error.message
+        assertNotNull(errorMessage)
+        assertContains(
+            errorMessage,
+            configuredCompression.toString(),
+        )
+        assertContains(
+            errorMessage,
+            fileCompression.toString(),
+        )
     }
 }

src/commonTest/kotlin/internal/NbtTagTypeTest.kt
@@ -1,88 +1,57 @@
 package net.benwoodworth.knbt.internal
 
-import io.kotest.property.Exhaustive
-import io.kotest.property.checkAll
-import io.kotest.property.exhaustive.bytes
-import io.kotest.property.exhaustive.enum
-import io.kotest.property.exhaustive.filter
-import kotlinx.coroutines.test.runTest
+import com.benwoodworth.parameterize.parameter
 import net.benwoodworth.knbt.*
-import net.benwoodworth.knbt.internal.NbtTagType.*
+import net.benwoodworth.knbt.test.filter
+import net.benwoodworth.knbt.test.parameterOfBytes
+import net.benwoodworth.knbt.test.parameterizeTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertNull
 
 class NbtTagTypeTest {
     @Test
-    fun converting_to_NbtTagType_or_null_from_a_valid_ID_byte_should_return_the_correct_tag() = runTest {
-        checkAll(Exhaustive.enum<NbtTagType>()) { expectedType ->
-            val id = expectedType.id
+    fun converting_to_NbtTagType_or_null_from_a_valid_ID_byte_should_return_the_correct_tag() = parameterizeTest {
+        val expectedType by parameter(NbtTagType.entries)
+        val id = expectedType.id
 
-            assertEquals(expectedType, id.toNbtTagTypeOrNull())
-        }
+        assertEquals(expectedType, id.toNbtTagTypeOrNull())
     }
 
     @Test
-    fun converting_to_NbtTagType_or_null_from_an_invalid_ID_byte_should_return_null() = runTest {
-        val validIds = NbtTagType.entries.map { it.id }
+    fun converting_to_NbtTagType_or_null_from_an_invalid_ID_byte_should_return_null() = parameterizeTest {
+        val invalidId by parameterOfBytes()
+            .filter { id ->
+                NbtTagType.entries.none { it.id == id }
+            }
 
-        val invalidIds = Exhaustive.bytes()
-            .filter { it !in validIds }
-
-        invalidIds.checkAll { id ->
-            assertNull(id.toNbtTagTypeOrNull())
-        }
+        assertNull(invalidId.toNbtTagTypeOrNull())
     }
 
     @Test
-    fun converting_class_Nothing_should_return_TAG_End(): Unit =
-        assertEquals(TAG_End, Nothing::class.toNbtTagType())
+    fun converting_class_should_return_the_correct_tag_type() = parameterizeTest {
+        fun NbtTagType.nbtTagClass() = when (this) {
+            NbtTagType.TAG_End -> Nothing::class
+            NbtTagType.TAG_Byte -> NbtByte::class
+            NbtTagType.TAG_Short -> NbtShort::class
+            NbtTagType.TAG_Int -> NbtInt::class
+            NbtTagType.TAG_Long -> NbtLong::class
+            NbtTagType.TAG_Float -> NbtFloat::class
+            NbtTagType.TAG_Double -> NbtDouble::class
+            NbtTagType.TAG_Byte_Array -> NbtByteArray::class
+            NbtTagType.TAG_String -> NbtString::class
+            NbtTagType.TAG_List -> NbtList::class
+            NbtTagType.TAG_Compound -> NbtCompound::class
+            NbtTagType.TAG_Int_Array -> NbtIntArray::class
+            NbtTagType.TAG_Long_Array -> NbtLongArray::class
+        }
 
-    @Test
-    fun converting_class_NbtByte_should_return_TAG_Int(): Unit =
-        assertEquals(TAG_Byte, NbtByte::class.toNbtTagType())
+        val expectedConversions = NbtTagType.entries.asSequence()
+            .map { it.nbtTagClass() to it }
 
-    @Test
-    fun converting_class_NbtShort_should_return_TAG_Short(): Unit =
-        assertEquals(TAG_Short, NbtShort::class.toNbtTagType())
+        val expectedConversion by parameter(expectedConversions)
+        val (nbtTagClass, nbtTagType) = expectedConversion
 
-    @Test
-    fun converting_class_NbtInt_should_return_TAG_Int(): Unit =
-        assertEquals(TAG_Int, NbtInt::class.toNbtTagType())
-
-    @Test
-    fun converting_class_NbtLong_should_return_TAG_Long(): Unit =
-        assertEquals(TAG_Long, NbtLong::class.toNbtTagType())
-
-    @Test
-    fun converting_class_NbtFloat_should_return_TAG_Float(): Unit =
-        assertEquals(TAG_Float, NbtFloat::class.toNbtTagType())
-
-    @Test
-    fun converting_class_NbtDouble_should_return_TAG_Double(): Unit =
-        assertEquals(TAG_Double, NbtDouble::class.toNbtTagType())
-
-    @Test
-    fun converting_class_NbtByteArray_should_return_TAG_Byte_Array(): Unit =
-        assertEquals(TAG_Byte_Array, NbtByteArray::class.toNbtTagType())
-
-    @Test
-    fun converting_class_NbtString_should_return_TAG_String(): Unit =
-        assertEquals(TAG_String, NbtString::class.toNbtTagType())
-
-    @Test
-    fun converting_class_NbtList_should_return_TAG_List(): Unit =
-        assertEquals(TAG_List, NbtList::class.toNbtTagType())
-
-    @Test
-    fun converting_class_NbtCompound_should_return_TAG_Compound(): Unit =
-        assertEquals(TAG_Compound, NbtCompound::class.toNbtTagType())
-
-    @Test
-    fun converting_class_NbtIntArray_should_return_TAG_Int_Array(): Unit =
-        assertEquals(TAG_Int_Array, NbtIntArray::class.toNbtTagType())
-
-    @Test
-    fun converting_class_NbtLongArray_should_return_TAG_Long_Array(): Unit =
-        assertEquals(TAG_Long_Array, NbtLongArray::class.toNbtTagType())
+        assertEquals(nbtTagType, nbtTagClass.toNbtTagType())
+    }
 }

TheFloodDragon/nbt

src/commonTest/kotlin/internal/BinaryNbtReaderTest.kt
@@ -1,13 +1,16 @@
 package net.benwoodworth.knbt.internal
 
+import com.benwoodworth.parameterize.parameter
 import kotlinx.serialization.decodeFromByteArray
 import kotlinx.serialization.encodeToByteArray
 import net.benwoodworth.knbt.*
 import net.benwoodworth.knbt.NbtVariant.Java
 import net.benwoodworth.knbt.okio.decodeFromBufferedSource
 import net.benwoodworth.knbt.test.TestSource
+import net.benwoodworth.knbt.test.assume
 import net.benwoodworth.knbt.test.file.nbtFiles
-import net.benwoodworth.knbt.test.parameterize
+import net.benwoodworth.knbt.test.parameterizeTest
+import net.benwoodworth.knbt.test.parameters.parameterOfNbtCompressions
 import okio.buffer
 import okio.use
 import kotlin.test.*
@@ -15,88 +18,83 @@ import kotlin.test.*
 @OptIn(OkioApi::class)
 class BinaryNbtReaderTest {
     @Test
-    fun should_decode_to_class_correctly() = parameterize(nbtFiles) {
+    fun should_decode_to_class_correctly() = parameterizeTest {
+        val nbtFile by parameter(nbtFiles)
+
         assertEquals(
-            expected = value,
-            actual = asSource().use { source ->
-                nbt.decodeFromBufferedSource(valueSerializer, source.buffer())
+            expected = nbtFile.value,
+            actual = nbtFile.asSource().use { source ->
+                nbtFile.nbt.decodeFromBufferedSource(nbtFile.valueSerializer, source.buffer())
             },
         )
     }
 
     @Test
-    fun should_decode_to_NbtTag_correctly() = parameterize(nbtFiles) {
+    fun should_decode_to_NbtTag_correctly() = parameterizeTest {
+        val nbtFile by parameter(nbtFiles)
+
         assertEquals(
-            expected = nbtTag,
-            actual = asSource().use { source ->
-                nbt.decodeFromBufferedSource(source.buffer())
+            expected = nbtFile.nbtTag,
+            actual = nbtFile.asSource().use { source ->
+                nbtFile.nbt.decodeFromBufferedSource(source.buffer())
             },
         )
     }
 
     @Test
-    fun should_not_read_more_from_source_than_necessary() = parameterize(nbtFiles) {
-        TestSource(asSource()).use { source ->
-            nbt.decodeFromBufferedSource<NbtTag>(source.buffer())
+    fun should_not_read_more_from_source_than_necessary() = parameterizeTest {
+        val nbtFile by parameter(nbtFiles)
+
+        TestSource(nbtFile.asSource()).use { source ->
+            nbtFile.nbt.decodeFromBufferedSource<NbtTag>(source.buffer())
             assertFalse(source.readPastEnd)
         }
     }
 
     @Test
-    fun should_not_close_source() = parameterize(nbtFiles) {
-        TestSource(asSource()).use { source ->
-            nbt.decodeFromBufferedSource<NbtTag>(source.buffer())
+    fun should_not_close_source() = parameterizeTest {
+        val nbtFile by parameter(nbtFiles)
+
+        TestSource(nbtFile.asSource()).use { source ->
+            nbtFile.nbt.decodeFromBufferedSource<NbtTag>(source.buffer())
             assertFalse(source.isClosed)
         }
     }
 
     @Test
-    fun should_fail_with_incorrect_NbtCompression_and_specify_mismatched_compressions() {
-        data class Parameters(
-            val configuredCompression: NbtCompression,
-            val fileCompression: NbtCompression,
-        )
-
+    fun should_fail_with_incorrect_NbtCompression_and_specify_mismatched_compressions() = parameterizeTest {
         val data = buildNbtCompound("root") {
             put("string", "String!")
         }
 
-        val compressions = listOf(
-            NbtCompression.None,
-            NbtCompression.Gzip,
-            NbtCompression.Zlib,
-        )
+        val configuredCompression by parameterOfNbtCompressions()
+        val fileCompression by parameterOfNbtCompressions()
+        assume(configuredCompression != fileCompression)
 
-        val mismatchedCompressions = compressions
-            .flatMap { a -> compressions.map { b -> Parameters(a, b) } }
-            .filter { (a, b) -> a !== b }
-
-        parameterize(mismatchedCompressions, { "Configured: $configuredCompression, File: $fileCompression" }) {
-            val decodingNbt = Nbt {
-                variant = Java
-                compression = configuredCompression
-            }
-
-            val encodingNbt = Nbt(decodingNbt) {
-                compression = fileCompression
-            }
-
-            val encoded = encodingNbt.encodeToByteArray<NbtTag>(data)
-
-            val error = assertFailsWith<NbtDecodingException> {
-                decodingNbt.decodeFromByteArray<NbtTag>(encoded)
-            }
-
-            val errorMessage = error.message
-            assertNotNull(errorMessage)
-            assertContains(
-                errorMessage,
-                configuredCompression.toString(),
-            )
-            assertContains(
-                errorMessage,
-                fileCompression.toString(),
-            )
+        val decodingNbt = Nbt {
+            variant = Java
+            compression = configuredCompression
         }
+
+        val encodingNbt = Nbt(decodingNbt) {
+            compression = fileCompression
+        }
+
+        val encoded = encodingNbt.encodeToByteArray<NbtTag>(data)
+
+        val error = assertFailsWith<NbtDecodingException> {
+            decodingNbt.decodeFromByteArray<NbtTag>(encoded)
+        }
+
+        val errorMessage = error.message
+        assertNotNull(errorMessage)
+        assertContains(
+            errorMessage,
+            configuredCompression.toString(),
+        )
+        assertContains(
+            errorMessage,
+            fileCompression.toString(),
+        )
     }
 }

src/commonTest/kotlin/internal/NbtTagTypeTest.kt
@@ -1,88 +1,55 @@
 package net.benwoodworth.knbt.internal
 
-import io.kotest.property.Exhaustive
-import io.kotest.property.checkAll
-import io.kotest.property.exhaustive.bytes
-import io.kotest.property.exhaustive.enum
-import io.kotest.property.exhaustive.filter
-import kotlinx.coroutines.test.runTest
+import com.benwoodworth.parameterize.parameter
 import net.benwoodworth.knbt.*
-import net.benwoodworth.knbt.internal.NbtTagType.*
+import net.benwoodworth.knbt.test.assume
+import net.benwoodworth.knbt.test.parameterOfBytes
+import net.benwoodworth.knbt.test.parameterizeTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertNull
 
 class NbtTagTypeTest {
     @Test
-    fun converting_to_NbtTagType_or_null_from_a_valid_ID_byte_should_return_the_correct_tag() = runTest {
-        checkAll(Exhaustive.enum<NbtTagType>()) { expectedType ->
-            val id = expectedType.id
+    fun converting_to_NbtTagType_or_null_from_a_valid_ID_byte_should_return_the_correct_tag() = parameterizeTest {
+        val expectedType by parameter(NbtTagType.entries)
+        val id = expectedType.id
 
-            assertEquals(expectedType, id.toNbtTagTypeOrNull())
-        }
+        assertEquals(expectedType, id.toNbtTagTypeOrNull())
     }
 
     @Test
-    fun converting_to_NbtTagType_or_null_from_an_invalid_ID_byte_should_return_null() = runTest {
-        val validIds = NbtTagType.entries.map { it.id }
+    fun converting_to_NbtTagType_or_null_from_an_invalid_ID_byte_should_return_null() = parameterizeTest {
+        val invalidId by parameterOfBytes()
+        assume(NbtTagType.entries.none { it.id == invalidId })
 
-        val invalidIds = Exhaustive.bytes()
-            .filter { it !in validIds }
-
-        invalidIds.checkAll { id ->
-            assertNull(id.toNbtTagTypeOrNull())
-        }
+        assertNull(invalidId.toNbtTagTypeOrNull())
     }
 
     @Test
-    fun converting_class_Nothing_should_return_TAG_End(): Unit =
-        assertEquals(TAG_End, Nothing::class.toNbtTagType())
+    fun converting_class_should_return_the_correct_tag_type() = parameterizeTest {
+        fun NbtTagType.nbtTagClass() = when (this) {
+            NbtTagType.TAG_End -> Nothing::class
+            NbtTagType.TAG_Byte -> NbtByte::class
+            NbtTagType.TAG_Short -> NbtShort::class
+            NbtTagType.TAG_Int -> NbtInt::class
+            NbtTagType.TAG_Long -> NbtLong::class
+            NbtTagType.TAG_Float -> NbtFloat::class
+            NbtTagType.TAG_Double -> NbtDouble::class
+            NbtTagType.TAG_Byte_Array -> NbtByteArray::class
+            NbtTagType.TAG_String -> NbtString::class
+            NbtTagType.TAG_List -> NbtList::class
+            NbtTagType.TAG_Compound -> NbtCompound::class
+            NbtTagType.TAG_Int_Array -> NbtIntArray::class
+            NbtTagType.TAG_Long_Array -> NbtLongArray::class
+        }
 
-    @Test
-    fun converting_class_NbtByte_should_return_TAG_Int(): Unit =
-        assertEquals(TAG_Byte, NbtByte::class.toNbtTagType())
+        val expectedConversions = NbtTagType.entries.asSequence()
+            .map { it.nbtTagClass() to it }
 
-    @Test
-    fun converting_class_NbtShort_should_return_TAG_Short(): Unit =
-        assertEquals(TAG_Short, NbtShort::class.toNbtTagType())
+        val expectedConversion by parameter(expectedConversions)
+        val (nbtTagClass, nbtTagType) = expectedConversion
 
-    @Test
-    fun converting_class_NbtInt_should_return_TAG_Int(): Unit =
-        assertEquals(TAG_Int, NbtInt::class.toNbtTagType())
-
-    @Test
-    fun converting_class_NbtLong_should_return_TAG_Long(): Unit =
-        assertEquals(TAG_Long, NbtLong::class.toNbtTagType())
-
-    @Test
-    fun converting_class_NbtFloat_should_return_TAG_Float(): Unit =
-        assertEquals(TAG_Float, NbtFloat::class.toNbtTagType())
-
-    @Test
-    fun converting_class_NbtDouble_should_return_TAG_Double(): Unit =
-        assertEquals(TAG_Double, NbtDouble::class.toNbtTagType())
-
-    @Test
-    fun converting_class_NbtByteArray_should_return_TAG_Byte_Array(): Unit =
-        assertEquals(TAG_Byte_Array, NbtByteArray::class.toNbtTagType())
-
-    @Test
-    fun converting_class_NbtString_should_return_TAG_String(): Unit =
-        assertEquals(TAG_String, NbtString::class.toNbtTagType())
-
-    @Test
-    fun converting_class_NbtList_should_return_TAG_List(): Unit =
-        assertEquals(TAG_List, NbtList::class.toNbtTagType())
-
-    @Test
-    fun converting_class_NbtCompound_should_return_TAG_Compound(): Unit =
-        assertEquals(TAG_Compound, NbtCompound::class.toNbtTagType())
-
-    @Test
-    fun converting_class_NbtIntArray_should_return_TAG_Int_Array(): Unit =
-        assertEquals(TAG_Int_Array, NbtIntArray::class.toNbtTagType())
-
-    @Test
-    fun converting_class_NbtLongArray_should_return_TAG_Long_Array(): Unit =
-        assertEquals(TAG_Long_Array, NbtLongArray::class.toNbtTagType())
+        assertEquals(nbtTagType, nbtTagClass.toNbtTagType())
+    }
 }