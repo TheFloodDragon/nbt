BenWoodworth/knbt

WIP Move named root check into encoder/decoder
nbtwriter/reader should be dumb, only validating the values read, and
leaving any other validation to the encoder/decoder

TODO ^document in writer/reader interfaces

capabilities part of `NbtFormat` instead of the `NbtWriter`/`NbtReader`
so that serializing to `NbtTag` has the same restrictions as the
format-specific encode/decode functions. This is useful for
consistency, knowing that the actual serialization will fail if the
NbtTag serialization does too, and allowing serialization issues to be
caught in tests.

TODO ^docs in `encodeTo`/`decodeFromNbtTag`

Removed check in BinaryNbtReader since it was already guarded in the
decoder

src/commonTest/kotlin/NbtNameTest.kt
@@ -1,11 +1,18 @@
 package net.benwoodworth.knbt
 
+import com.benwoodworth.parameterize.parameter
+import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.builtins.ListSerializer
+import net.benwoodworth.knbt.internal.NbtDecodingException
+import net.benwoodworth.knbt.internal.nbtName
+import net.benwoodworth.knbt.test.filter
 import net.benwoodworth.knbt.test.parameterizeTest
+import net.benwoodworth.knbt.test.parameters.parameterOfDecoderVerifyingNbt
 import net.benwoodworth.knbt.test.parameters.parameterOfVerifyingNbt
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
 
 class NbtNameTest {
     @Serializable
@@ -71,6 +78,61 @@ class NbtNameTest {
             testDecodedValue = { value, decodedValue ->
                 assertEquals(value, decodedValue, "decodedValue")
             }
+        )
+    }
+
+
+    private data class SerializableValueWithNbtName<T>(
+        val value: T,
+        val serializer: KSerializer<T>,
+    )
+
+    private val valuesWithStaticNbtNames = buildList {
+        @Serializable
+        @NbtName("Name")
+        class Value {
+            override fun toString(): String = "Value"
+            override fun equals(other: Any?): Boolean = other is Value
+            override fun hashCode(): Int = this::class.hashCode()
+        }
+
+        add(SerializableValueWithNbtName(Value(), Value.serializer()))
+
+
+        @Serializable
+        @NbtName("DifferentName")
+        class DifferentValue {
+            override fun toString(): String = "DifferentValue"
+            override fun equals(other: Any?): Boolean = other is DifferentValue
+            override fun hashCode(): Int = this::class.hashCode()
+        }
+
+        add(SerializableValueWithNbtName(DifferentValue(), DifferentValue.serializer()))
+    }.let {
+        @Suppress("UNCHECKED_CAST")
+        it as List<SerializableValueWithNbtName<Any?>> // KT-68606: Remove cast
+    }
+
+    @Test
+    fun decoding_value_with_static_NBT_name_should_fail_with_different_name() = parameterizeTest {
+        val nbt by parameterOfDecoderVerifyingNbt(includeNamedRootNbt = true)
+            .filter { it.capabilities.namedRoot }
+
+        val value by parameter(valuesWithStaticNbtNames)
+        val name = value.serializer.descriptor.nbtName!!
+
+        val differentlyNamedNbtTag = buildNbtCompound("different_than_$name") {
+            // No elements, since the decoder should fail before reaching this point anyway
+        }
+
+        val failure = assertFailsWith<NbtDecodingException> {
+            nbt.verifyDecoder(value.serializer, differentlyNamedNbtTag)
+        }
+
+        assertEquals(
+            "Expected tag named '$name', but got '${differentlyNamedNbtTag.content.keys.single()}'",
+            failure.message,
+            "failure message"
         )
     }
 }

src/commonTest/kotlin/internal/NbtWriterEncoderTest.kt
@@ -1,0 +1,42 @@
+package net.benwoodworth.knbt.internal
+
+import com.benwoodworth.parameterize.parameter
+import net.benwoodworth.knbt.NbtCompound
+import net.benwoodworth.knbt.NbtTag
+import net.benwoodworth.knbt.buildNbtCompound
+import net.benwoodworth.knbt.internal.NbtTagType.TAG_Compound
+import net.benwoodworth.knbt.put
+import net.benwoodworth.knbt.test.filter
+import net.benwoodworth.knbt.test.parameterizeTest
+import net.benwoodworth.knbt.test.parameters.parameterOfEncoderVerifyingNbt
+import net.benwoodworth.knbt.test.parameters.parameterOfNbtTagTypeEdgeCases
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
+
+class NbtWriterEncoderTest {
+    @Test
+    fun named_root_formats_should_throw_when_encoding_a_tag_that_is_not_a_compound_with_one_entry() = parameterizeTest {
+        val nbt by parameterOfEncoderVerifyingNbt(includeNamedRootNbt = true)
+            .filter { it.capabilities.namedRoot }
+
+        val tag by parameter {
+            sequence {
+                yieldAll(
+                    this@parameterizeTest.parameterOfNbtTagTypeEdgeCases().arguments
+                        .filter { it !is NbtCompound }
+                )
+
+                yield(buildNbtCompound { /* no entries */ })
+
+                yield(buildNbtCompound { put("1", 1); put("2", 2) })
+            }
+        }
+
+        val failure = assertFailsWith<NbtEncodingException> {
+            nbt.verifyEncoder(NbtTag.serializer(), tag, tag)
+        }
+
+        assertEquals("The ${nbt.name} format only supports $TAG_Compound with one entry", failure.message)
+    }
+}

TheFloodDragon/nbt

src/commonTest/kotlin/NbtNameTest.kt
@@ -1,11 +1,18 @@
 package net.benwoodworth.knbt
 
+import com.benwoodworth.parameterize.parameter
+import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.builtins.ListSerializer
+import net.benwoodworth.knbt.internal.NbtDecodingException
+import net.benwoodworth.knbt.internal.nbtName
+import net.benwoodworth.knbt.test.assume
 import net.benwoodworth.knbt.test.parameterizeTest
+import net.benwoodworth.knbt.test.parameters.parameterOfDecoderVerifyingNbt
 import net.benwoodworth.knbt.test.parameters.parameterOfVerifyingNbt
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
 
 class NbtNameTest {
     @Serializable
@@ -71,6 +78,61 @@ class NbtNameTest {
             testDecodedValue = { value, decodedValue ->
                 assertEquals(value, decodedValue, "decodedValue")
             }
+        )
+    }
+
+
+    private data class SerializableValueWithNbtName<T>(
+        val value: T,
+        val serializer: KSerializer<T>,
+    )
+
+    private val valuesWithStaticNbtNames = buildList {
+        @Serializable
+        @NbtName("Name")
+        class Value {
+            override fun toString(): String = "Value"
+            override fun equals(other: Any?): Boolean = other is Value
+            override fun hashCode(): Int = this::class.hashCode()
+        }
+
+        add(SerializableValueWithNbtName(Value(), Value.serializer()))
+
+
+        @Serializable
+        @NbtName("DifferentName")
+        class DifferentValue {
+            override fun toString(): String = "DifferentValue"
+            override fun equals(other: Any?): Boolean = other is DifferentValue
+            override fun hashCode(): Int = this::class.hashCode()
+        }
+
+        add(SerializableValueWithNbtName(DifferentValue(), DifferentValue.serializer()))
+    }.let {
+        @Suppress("UNCHECKED_CAST")
+        it as List<SerializableValueWithNbtName<Any?>> // KT-68606: Remove cast
+    }
+
+    @Test
+    fun decoding_value_with_static_NBT_name_should_fail_with_different_name() = parameterizeTest {
+        val nbt by parameterOfDecoderVerifyingNbt(includeNamedRootNbt = true)
+        assume(nbt.capabilities.namedRoot)
+
+        val value by parameter(valuesWithStaticNbtNames)
+        val name = value.serializer.descriptor.nbtName!!
+
+        val differentlyNamedNbtTag = buildNbtCompound("different_than_$name") {
+            // No elements, since the decoder should fail before reaching this point anyway
+        }
+
+        val failure = assertFailsWith<NbtDecodingException> {
+            nbt.verifyDecoder(value.serializer, differentlyNamedNbtTag)
+        }
+
+        assertEquals(
+            "Expected tag named '$name', but got '${differentlyNamedNbtTag.content.keys.single()}'",
+            failure.message,
+            "failure message"
         )
     }
 }

src/commonTest/kotlin/internal/NbtWriterEncoderTest.kt
@@ -1,0 +1,42 @@
+package net.benwoodworth.knbt.internal
+
+import com.benwoodworth.parameterize.parameter
+import net.benwoodworth.knbt.NbtCompound
+import net.benwoodworth.knbt.NbtTag
+import net.benwoodworth.knbt.buildNbtCompound
+import net.benwoodworth.knbt.internal.NbtTagType.TAG_Compound
+import net.benwoodworth.knbt.put
+import net.benwoodworth.knbt.test.assume
+import net.benwoodworth.knbt.test.parameterizeTest
+import net.benwoodworth.knbt.test.parameters.parameterOfEncoderVerifyingNbt
+import net.benwoodworth.knbt.test.parameters.parameterOfNbtTagTypeEdgeCases
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
+
+class NbtWriterEncoderTest {
+    @Test
+    fun named_root_formats_should_throw_when_encoding_a_tag_that_is_not_a_compound_with_one_entry() = parameterizeTest {
+        val nbt by parameterOfEncoderVerifyingNbt(includeNamedRootNbt = true)
+        assume(nbt.capabilities.namedRoot)
+
+        val tag by parameter {
+            sequence {
+                yieldAll(
+                    this@parameterizeTest.parameterOfNbtTagTypeEdgeCases().arguments
+                        .filter { it !is NbtCompound }
+                )
+
+                yield(buildNbtCompound { /* no entries */ })
+
+                yield(buildNbtCompound { put("1", 1); put("2", 2) })
+            }
+        }
+
+        val failure = assertFailsWith<NbtEncodingException> {
+            nbt.verifyEncoder(NbtTag.serializer(), tag, tag)
+        }
+
+        assertEquals("The ${nbt.name} format only supports $TAG_Compound with one entry", failure.message)
+    }
+}