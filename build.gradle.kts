f60819f589c78da53b988619b6839b881183f757
蛟龙 <1610105206@qq.com>
2025-03-15 00:03:33 +0800

    publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,8 +46,8 @@ publishing {
             name = "GitHubPackages"
             url = uri("https://maven.pkg.github.com/TheFloodDragon/nbt")
             credentials {
-                username = System.getenv("GITHUB_USER")
-                password = System.getenv("GITHUB_TOKEN")
+                username = project.findProperty("github.user")?.toString() ?: System.getenv("GITHUB_USER")
+                password = project.findProperty("github.token")?.toString() ?: System.getenv("GITHUB_TOKEN")
             }
         }
     }
